2ae5e49500af8c0ba625d83f3fc14aac026e6bd8
Ryan Chae <k_1108@hotmail.com>
2020-04-19 22:18:54 -0400

    updated header

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 docker build -t chae1108/multi-client:latest -t chae1108/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t chae1108/multi-server:latest -t chae1108/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t chae1108/multi-worker:latest -t chae1108/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push chae1108/mult-client:latest
-docker push chae1108/mult-server:latest
-docker push chae1108/mult-worker:latest
+docker push chae1108/multi-client:latest
+docker push chae1108/multi-server:latest
+docker push chae1108/multi-worker:latest
 
-docker push chae1108/mult-client:$SHA
-docker push chae1108/mult-server:$SHA
-docker push chae1108/mult-worker:$SHA
+docker push chae1108/multi-client:$SHA
+docker push chae1108/multi-server:$SHA
+docker push chae1108/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=chae1108/multi-server:$SHA
