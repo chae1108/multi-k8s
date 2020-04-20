fb0a138ce7b4fa8960149271d609dbf11ab76fc7
Ryan Chae <k_1108@hotmail.com>
2020-04-19 21:42:48 -0400

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t chae1108/multi-client:latest -t chae1108/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t chae1108/multi-server:latest -t chae1108/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t chae1108/multi-worker:latest -t chae1108/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push chae1108/mult-client:latest
+docker push chae1108/mult-server:latest
+docker push chae1108/mult-worker:latest
+
+docker push chae1108/mult-client:$SHA
+docker push chae1108/mult-server:$SHA
+docker push chae1108/mult-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=chae1108/multi-server:$SHA
+kubectl set image deployments/client-deployment client=chae1108/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=chae1108/multi-worker:$SHA
